9ece1fc30c910bc0b27a747079c7ebcadd02a373
Monoshi-tonmoy <44216787+Monoshi-tonmoy@users.noreply.github.com>
2020-10-31 15:31:47 +0600

    This was my 2-1 project, a simple breakout game using java.

---
+++ src/breakout/Board.java
@@ -0,0 +1,274 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package breakout;
+
+import static breakout.Breakout.playsound;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JPanel;
+
+public class Board extends JPanel implements Commons {
+
+    private Timer timer;
+    private String message = "YOU LOSE";
+    private Ball ball;
+    private Paddle paddle;
+    private Brick bricks[];
+    private boolean ingame = true;
+    private int flag1=0;
+    private int flag2=0;
+   // private int flag3=0;
+    private Clip clip;
+    
+
+    public Board() {
+
+        initBoard();
+    }
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+
+        bricks = new Brick[N_OF_BRICKS];
+        setDoubleBuffered(true);
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ScheduleTask(), DELAY, PERIOD);
+    }
+
+    @Override
+    public void addNotify() {
+
+        super.addNotify();
+        gameInit();
+    }
+
+    private void gameInit() {
+
+        ball = new Ball();
+        paddle = new Paddle();
+
+        int k = 0;
+        for (int i = 0; i < 17;i++) {
+            for (int j = 0; j <15; j++) {
+                bricks[k] = new Brick(j * 40 + 30, i * 10 + 40);
+                k++;
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+
+        if (ingame) {
+            
+            drawObjects(g2d);
+        } else {
+
+            gameFinished(g2d);
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+    
+    private void drawObjects(Graphics2D g2d) {
+        
+        g2d.drawImage(ball.getImage(), ball.getX(), ball.getY(),
+                ball.getWidth(), ball.getHeight(), this);
+        g2d.drawImage(paddle.getImage(), paddle.getX(), paddle.getY(),
+                paddle.getWidth(), paddle.getHeight(), this);
+
+        for (int i = 0; i < N_OF_BRICKS; i++) {
+            if (!bricks[i].isDestroyed()) {
+                g2d.drawImage(bricks[i].getImage(), bricks[i].getX(),
+                        bricks[i].getY(), bricks[i].getWidth(),
+                        bricks[i].getHeight(), this);
+            }
+        }
+    }
+    
+    private void gameFinished(Graphics2D g2d) {
+
+        Font font = new Font("Verdana", Font.BOLD, 18);
+        FontMetrics metr = this.getFontMetrics(font);
+
+        g2d.setColor(Color.BLUE);
+        g2d.setFont(font);
+        g2d.drawString(message,
+                (Commons.WIDTH - metr.stringWidth(message)) / 2,
+                Commons.WIDTH / 2);
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            paddle.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            paddle.keyPressed(e);
+        }
+    }
+
+    private class ScheduleTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            ball.move();
+            paddle.move();
+            checkCollision();
+            repaint();
+        }
+    }
+
+    private void stopGame() {
+
+        ingame = false;
+        timer.cancel();
+    }
+    public static void play(File Sound){
+        try{
+            Clip clip=AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(Sound));
+            clip.start();
+            
+            
+           // Thread.sleep(10);
+        }catch(Exception e){
+            
+        }
+    }
+    public void stop(){
+        clip.stop();
+    }
+
+    private void checkCollision() {
+
+        if (ball.getRect().getMaxY() > Commons.BOTTOM_EDGE) {
+            stopGame();
+        }
+
+        for (int i = 0, j = 0; i < N_OF_BRICKS; i++) {
+            
+            if (bricks[i].isDestroyed()) {
+                j++;
+                //File clap=new File("Brick.wav");
+                //play(clap);
+                //stop();
+                //clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            if(j==20&flag1==0){
+                timer.scheduleAtFixedRate(new ScheduleTask(), 100, 8);
+                flag1=1;
+            }
+            if(j==100&flag2==0){
+                timer.scheduleAtFixedRate(new ScheduleTask(), 100, 7);
+                flag2=1;
+            }
+            
+            if (j == N_OF_BRICKS) {
+                message = "You WIn";
+                stopGame();
+            }
+        }
+
+        if ((ball.getRect()).intersects(paddle.getRect())) {
+
+            int paddleLPos = (int) paddle.getRect().getMinX();
+            int ballLPos = (int) ball.getRect().getMinX();
+
+            int first = paddleLPos +25;
+            int second = paddleLPos + 50;
+            int third = paddleLPos + 75;
+            int fourth = paddleLPos + 100;
+
+            if (ballLPos < first) {
+                ball.setXDir(-1);
+                ball.setYDir(-1);
+            }
+
+            if (ballLPos >= first && ballLPos < second) {
+                ball.setXDir(-1);
+                ball.setYDir(-1 * ball.getYDir());
+            }
+
+            if (ballLPos >= second && ballLPos < third) {
+                ball.setXDir(0);
+                ball.setYDir(-1);
+            }
+
+            if (ballLPos >= third && ballLPos < fourth) {
+                ball.setXDir(1);
+                ball.setYDir(-1 * ball.getYDir());
+            }
+
+            if (ballLPos > fourth) {
+                ball.setXDir(1);
+                ball.setYDir(-1);
+            }
+        }
+
+        for (int i = 0; i < N_OF_BRICKS; i++) {
+            
+            if ((ball.getRect()).intersects(bricks[i].getRect())) {
+
+                int ballLeft = (int) ball.getRect().getMinX();
+                int ballHeight = (int) ball.getRect().getHeight();
+                int ballWidth = (int) ball.getRect().getWidth();
+                int ballTop = (int) ball.getRect().getMinY();
+
+                Point pointRight = new Point(ballLeft + ballWidth + 1, ballTop);
+                Point pointLeft = new Point(ballLeft - 1, ballTop);
+                Point pointTop = new Point(ballLeft, ballTop - 1);
+                Point pointBottom = new Point(ballLeft, ballTop + ballHeight + 1);
+
+                if (!bricks[i].isDestroyed()) {
+                    if (bricks[i].getRect().contains(pointRight)) {
+                        ball.setXDir(-1);
+                    } else if (bricks[i].getRect().contains(pointLeft)) {
+                        ball.setXDir(1);
+                    }
+
+                    if (bricks[i].getRect().contains(pointTop)) {
+                        ball.setYDir(1);
+                    } else if (bricks[i].getRect().contains(pointBottom)) {
+                        ball.setYDir(-1);
+                    }
+
+                    bricks[i].setDestroyed(true);
+                }
+            }
+        }
+    }
+    
+   
+}
---
+++ src/breakout/Commons.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package breakout;
+
+public interface Commons {
+    
+    public static final int WIDTH = 670;
+    public static final int HEIGTH = 600;
+    public static final int BOTTOM_EDGE = 590;
+    public static final int N_OF_BRICKS = 255;
+    public static final int INIT_PADDLE_X = 305;
+    public static final int INIT_PADDLE_Y = 560;
+    public static final int INIT_BALL_X = 350;
+    public static final int INIT_BALL_Y = 550;    
+    public static final int DELAY = 100;
+    public static final int PERIOD = 9;
+}
